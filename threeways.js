745138a13204b0b507b58516ec81ac5ed538f0d4
lpoghi <85479967+lpoghi@users.noreply.github.com>
2021-08-23 13:17:03 +0200

    Add files via upload

---
+++ threeways.js
@@ -0,0 +1,24 @@
+function doSquareCalculation(number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+}
+
+
+const doSquareCalculation = function (number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+
+const doSquareCalculation = (number1, number2) => {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
